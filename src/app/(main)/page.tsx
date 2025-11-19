d72f798394a07885cbb5d0989597dcd234037058
Hüseyn Tahirov <huseyntahirovndu@gmail.com>
2025-11-19 20:11:23 +0000

    Loqonun linki https://i.ibb.co/1wN1rhJ/NAX-VAN-D-VL-T-UN-VERS-TET-N-N-N-

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -120,7 +120,7 @@ export default function HomePage() {
       {/* Hero Section */}
       <section className="relative w-full h-screen">
           <Image
-            src="https://i.ibb.co/yFjxDz0w/q1.jpg"
+            src="https://i.ibb.co/cXv2KzRR/q2.jpg"
             alt="Naxçıvan Dövlət Universiteti"
             fill
             className="object-cover"
--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,7 +4,7 @@ import { cn } from '@/lib/utils';
 export function Logo(props: ImgHTMLAttributes<HTMLImageElement>) {
   return (
     <img
-      src="https://i.ibb.co/yFjxDz0w/q1.jpg"
+      src="https://i.ibb.co/1wN1rhJ/NAX-VAN-D-VL-T-UN-VERS-TET-N-N-N-ZD-ND-G-MNAZ-YA.png"
       alt="Naxçıvan Dövlət Universiteti Logo"
       {...props}
       className={cn("h-10 w-auto", props.className)}
